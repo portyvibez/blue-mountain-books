5751df2f1ad2da622591c2e20aaf3a383d741696
Khashana McKenzie <khashanamckenzie@MacBookPro.attlocal.net>
2026-05-06 09:30:09 -0400

    Fix TypeScript React build

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,15 @@
-import { StrictMode } from "react"; import { createRoot } from "react-dom/client"; import App from "./App.jsx"; createRoot(document.getElementById("root")).render(<StrictMode><App /></StrictMode>);
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
